6e3d9aababe77103c8b13177df5c82ec078e8caf
ThunderProgrammer777 <shloksoni2007@gmail.com>
2020-12-31 13:16:22 +0530

    improve

--- time.py
+++ time.py
@@ -1,10 +1,10 @@
 import math
-a = int(input(" write the coefficient of x^2:"))
-b = int(input("write the coefficient of x: "))
-c = int(input("write constant (c):"))
+a = int(input("Write the coefficient of x^2:"))
+b = int(input("Write the coefficient of x: "))
+c = int(input("Write constant (c):"))
 disc = math.sqrt(b**2-(4*a*c))
 print(disc)
 x1 = ((-b+disc)/2*a)
 x2 = ((-b-disc)/2*a)
 
-print(f"x can be {x1} or {x2}.")
+print(f"Roots are {x1} or {x2}.")

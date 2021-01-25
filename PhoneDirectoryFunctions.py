605d7ee052cf040ef30c6ae32bd49c03c4b71889
ThunderProgrammer777 <shloksoni2007@gmail.com>
2021-01-25 18:41:58 +0530

    fixed bug

--- PhoneDirectoryFunctions.py
+++ PhoneDirectoryFunctions.py
@@ -1,37 +1,36 @@
 import json
+
+
 class Phone:
     @staticmethod
-    def add(name:str,phone:int):
+    def add(name: str, phone: int):
         with open('phone.txt', 'r') as file:
             books = json.load(file)
         with open('phone.txt', 'w') as file:
-            books.append({"name": name,"phone":phone})
+            books.append({"name": name, "phone": phone})
             json.dump(books, file)
 
     @staticmethod
-    def remove(name:str):
+    def remove(name: str):
         with open('phone.txt', 'r') as file:
             books = json.load(file)
         for book in books:
-            if book['name']==name:
+            if book['name'] == name:
                 books.remove(book)
         with open('phone.txt', 'w') as file:
-            json.dump(books,file)
+            json.dump(books, file)
 
     @staticmethod
-    def find(name:str):
+    def find(name: str):
         with open('phone.txt', 'r') as file:
             books = json.load(file)
             for book in books:
                 if book['name'] == name:
                     print(f"{book['name']} Ph.{book['phone']}")
+
     @staticmethod
     def allnum():
         with open('phone.txt', 'r') as file:
             books = json.load(file)
             for book in books:
                 print(f"{book['name']} Ph.{book['phone']}")
-
-
-
-
--- PhoneDirectoryRun.py
+++ PhoneDirectoryRun.py
@@ -1,7 +1,5 @@
 from PhoneDirectoryFunctions import Phone as ph
 
-
-
 Choice = ("""
 l = list of all numbers
 a = add number
@@ -9,18 +7,18 @@ f = find a number
 d = delete a number
 q = quit
 Your Choice = """)
-choice=str(input(Choice))
-while choice!='q':
-    if choice=='l':
+choice = str(input(Choice))
+while choice != 'q':
+    if choice == 'l':
         ph.allnum()
-    elif choice=='a':
+    elif choice == 'a':
         name = input("Enter the name: ")
         phone = int(input("Enter the phone number: "))
-        ph.add(name,phone)
-    elif choice=='f':
+        ph.add(name, phone)
+    elif choice == 'f':
         na = input("Enter the name of the person whose number you want to find: ")
         ph.find(na)
-    elif choice=='d':
+    elif choice == 'd':
         nam = input("Enter the name of the person whose number you want to delete: ")
         ph.remove(nam)
-    choice=str(input(Choice))
+    choice = str(input(Choice))

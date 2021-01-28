a6296ddf7fcd51be861633879e2779070c671bb9
ThunderProgrammer777 <shloksoni2007@gmail.com>
2021-01-28 19:19:25 +0530

    dealer funtions completed and dealer function can run smoothly now :)

--- dealer_func.py
+++ dealer_func.py
@@ -1,39 +1,67 @@
 import json
 
-password = ("xyz")
+password = "xyz"
+
 
 class Dealer:
     @staticmethod
-    def add(name, brand,mileage,fuel1,fuel_capacity,seater,engine,price1,price2):
-        fuel2=str(input("Is this car can run on any other fuel: "))
+    def add(name, brand, mileage, fuel1, fuel_capacity, seater, engine, price1, price2, stock):
+        fuel2 = str(input("Is this car can run on any other fuel: "))
         with open('cars.txt', 'r') as file:
             car = json.load(file)
         with open('cars.txt', 'w') as file:
-            if fuel2=="no":
-                car.append({"name": name, "brand": brand, "mileage":mileage, "fuel1":fuel1, "fuel_capacity": fuel_capacity, "seater":seater, "engine":engine, "price1":price1, "price2":price2})
+            if fuel2 == "no":
+                car.append(
+                    {"name": name, "brand": brand, "mileage": mileage, "fuel1": fuel1, "fuel_capacity": fuel_capacity,
+                     "seater": seater, "engine": engine, "price1": price1, "price2": price2, "stock": stock})
                 json.dump(car, file)
+            elif fuel2 == "yes":
+                fuel = input("On which other fuel does this model run: ")
+                car.append(
+                    {"name": name, "brand": brand, "mileage": mileage, "fuel1": fuel1,'fuel2':fuel, "fuel_capacity": fuel_capacity,
+                     "seater": seater, "engine": engine, "price1": price1, "price2": price2, "stock": stock})
+                json.dump(car, file)
+
+
+    @staticmethod
+    def stock_of_car():
+        name = input("Enter the name of the car: ")
+        st = input("Yes/No: ")
+        with open('cars.txt', 'r') as file:
+            cars = json.load(file)
+            for car in cars:
+                if car["name"] == name:
+                    car['stock'] = st
+        with open('cars.txt', 'w') as fo:
+            json.dump(cars, fo)
 
     @staticmethod
     def remove(name: str):
         with open('cars.txt', 'r') as file:
-            books = json.load(file)
-        for book in books:
-            if book['name'] == name:
-                books.remove(book)
+            cars = json.load(file)
+        for car in cars:
+            if car['name'] == name:
+                cars.remove(car)
         with open('cars.txt', 'w') as file:
-            json.dump(books, file)
+            json.dump(cars, file)
 
     @staticmethod
     def find(name: str):
         with open('cars.txt', 'r') as file:
-            books = json.load(file)
-            for book in books:
-                if book['name'] == name:
-                    print(f"{book['name']} Ph.{book['phone']}")
+            cars = json.load(file)
+            for car in cars:
+                if car['name'] == name:
+                    stock = car['stock'].capitalize()
+                    print(
+                        f"{car['name']} of {car['brand']} brand. Has mileage of {car['mileage']}km/l. It can run on {car['fuel1']}.Has a fuel capacity of {car['fuel_capacity']}l.It have {car['seater']} seats. It have {car['engine']} engine. Price is from {car['price1']} l to {car['price2']} l. Stock:{stock}")
 
     @staticmethod
     def allnum():
         with open('cars.txt', 'r') as file:
-            books = json.load(file)
-            for book in books:
-                print(f"{book['name']} Ph.{book['phone']}")
+            cars = json.load(file)
+            for car in cars:
+                stock = car['stock'].capitalize()
+                try:
+                    print(f"{car['name']} of {car['brand']} brand. Has mileage of {car['mileage']}km/l. It can run on {car['fuel1']} and {car['fuel2']}.Has a fuel capacity of {car['fuel_capacity']}l.It have {car['seater']} seats. It have {car['engine']} engine. Price is from {car['price1']} l to {car['price2']} l. Stock:{stock}")
+                except:
+                    print(f"{car['name']} of {car['brand']} brand. Has mileage of {car['mileage']}km/l. It can run on {car['fuel1']}.Has a fuel capacity of {car['fuel_capacity']}l.It have {car['seater']} seats. It have {car['engine']} engine. Price is from {car['price1']} l to {car['price2']} l. Stock:{stock}")
--- run.py
+++ run.py
@@ -1,31 +1,44 @@
 from dealer_func import Dealer as ph
+from dealer_func import password as pas
 
-Choice = ("""
-l = list of all numbers
+Choice1 = ("""
+l = list of all car model
 a = add number
-f = find a number
-d = delete a number
+f = find a car model
+d = delete a car model
+s = mark the car in stock or not
 q = quit
 Your Choice = """)
-choice = str(input(Choice))
-while choice != 'q':
-    if choice == 'l':
-        ph.allnum()
-    elif choice == 'a':
-        name = input("Enter the name: ")
-        brand = str(input("Enter the brand: "))
-        mileage = float(input("What is the mileage of car per litre in km: "))
-        fuel = str(input("Enter the type of fuel can used in this model: "))
-        fuel_capacity = float(input("What is the fuel capacity of this model in litre: "))
-        seater = int(input("How many seat are present in this car model: "))
-        engine = input("Enter the name of the engine present in the car model: ")
-        price1 = float(input("Enter the minimum price of the model of the car in lakh: "))
-        price2 = float(input("Enter the maximum price of the model of the car in lakh: "))
-        ph.add(name, brand,mileage,fuel,fuel_capacity,seater,engine,price1,price2)
-    elif choice == 'f':
-        na = input("Enter the name of the person whose number you want to find: ")
-        ph.find(na)
-    elif choice == 'd':
-        nam = input("Enter the name of the person whose number you want to delete: ")
-        ph.remove(nam)
-    choice = str(input(Choice))
+person=str(input("Who is login to this program customer or dealer: "))
+if person=='dealer':
+    passs = input("Enter the password: ")
+    if passs == pas:
+        choice = str(input(Choice1))
+        while choice != 'q':
+            if choice == 'l':
+                ph.allnum()
+            elif choice == 'a':
+                name = input("Enter the name of the model of the car: ")
+                brand = str(input("Enter the brand name: "))
+                mileage = float(input("What is the mileage of car per litre in km: "))
+                fuel = str(input("Enter the type of fuel can used in this model: "))
+                fuel_capacity = float(input("What is the fuel capacity of this model in litre: "))
+                seater = int(input("How many seat are present in this car model: "))
+                engine = input("Enter the name of the engine present in the car model: ")
+                price1 = float(input("Enter the minimum price of the model of the car in lakh: "))
+                price2 = float(input("Enter the maximum price of the model of the car in lakh: "))
+                stock = str(input("Is this model in stock?[yes/no]:"))
+                ph.add(name, brand, mileage, fuel, fuel_capacity, seater, engine, price1, price2, stock)
+            elif choice == 'f':
+                na = input("Enter the name of the car model which you want to find: ")
+                ph.find(na)
+            elif choice == 'd':
+                nam = input("Enter the name of the car which you want to remove: ")
+                ph.remove(nam)
+            elif choice == 's':
+                ph.stock_of_car()
+            choice = str(input(Choice1))
+    else:
+        print("Wrong Password!!!")
+elif person=='customer':
+    pass
